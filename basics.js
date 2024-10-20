b563b27175e33b3ac062ebad3848d24f9cdf2882
ThatBeaverDev <107750535+ThatBeaverDev@users.noreply.github.com>
2024-10-20 12:52:04 +0100

    Some changes to the site

--- basics.js
+++ basics.js
@@ -5,3 +5,10 @@ fetch('/nav.html')
         document.getElementById('navbar-placeholder').innerHTML = data;
     })
     .catch(error => console.error('Error loading navbar:', error));
+
+fetch('/footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('footer-placeholder').innerHTML = data;
+    })
+    .catch(error => console.error('Error loading navbar:', error));
